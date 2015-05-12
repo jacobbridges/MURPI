cf0afec74f68cbf9330b15400301dcda38780680
jacobbridges <him@jacobandkate143.com>
2015-05-11 22:26:03 -0400

    added the model code which was in core

--- rpg_mechanics/models.py
+++ rpg_mechanics/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from murpi_core.models import Race, Character
 
-# Create your models here.
+
+class Class(models.Model):
+    name = models.CharField(max_length=50)
+    race = models.ForeignKey(Race, null=True)
+    description = models.TextField(blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+class Mastery(models.Model):
+    name = models.name = models.CharField(max_length=50)
+    race = models.ForeignKey(Race, null=True)
+    description = models.TextField(blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+class Ability(models.Model):
+    name = models.name = models.CharField(max_length=50)
+    description = models.TextField(blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+class Skill(models.Model):
+    name = models.name = models.CharField(max_length=50)
+    description = models.TextField(blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+class Trait(models.Model):
+    name = models.name = models.CharField(max_length=50)
+    description = models.TextField(blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+class CharacterTraits(models.Model):
+    character = models.ForeignKey(Character, null=False)
+    trait = models.ForeignKey(Trait, null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+class CharacterSkills(models.Model):
+    character = models.ForeignKey(Character, null=False)
+    skill = models.ForeignKey(Skill, null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+class CharacterAbilities(models.Model):
+    character = models.ForeignKey(Character, null=False)
+    ability = models.ForeignKey(Ability, null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
