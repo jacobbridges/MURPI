5464b5a78779197ed6a0e3c49ab44967e5c9746e
jacobbridges <him@jacobandkate143.com>
2015-05-12 22:52:28 -0400

    enabled the MEDIA_ROOT and MEDIA_URL

--- MURPI/settings.py
+++ MURPI/settings.py
@@ -106,6 +106,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+
+MEDIA_URL = '/media/'
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
