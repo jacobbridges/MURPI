87251c9ef2257437bf0efec6f4287282cc03baf4
jacobbridges <him@jacobandkate143.com>
2015-05-13 23:19:57 -0400

    testing the get_object_or_404 method

--- murpi_core/views.py
+++ murpi_core/views.py
@@ -1,11 +1,8 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Player
 
 
 def player_details(request, player_id):
-    try:
-        player = Player.objects.get(pk=player_id)
-    except Player.DoesNotExist:
-        raise Http404("Player does not exist")
+    player = get_object_or_404(Player, pk=player_id)
     return render(request, "player.html", {'player': player})
