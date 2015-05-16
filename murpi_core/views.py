4cc8e4a78d378346fa08e868334cc1ea574d7d2f
jacobbridges <him@jacobandkate143.com>
2015-05-16 13:22:47 -0400

    testing player register/login views

--- murpi_core/views.py
+++ murpi_core/views.py
@@ -2,28 +2,39 @@ from django.views.decorators.http import require_safe, require_POST
 from django.contrib.auth import authenticate
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, render, redirect
-from utilities.helpers import dict_has_keys
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+
+from .utils.helpers import dict_has_keys
 from .models import Player
 
 
-@require_safe()
-def retrieve_player(request, player_name):
-    player = get_object_or_404(Player, name=player_name)
-    return render(request, "player.html", {'player': player})
+@require_safe
+def retrieve_player(request, username):
+    user = get_object_or_404(User, username=username)
+    player = get_object_or_404(Player, user=user)
+    return render(request, "murpi_core/player.html", {'player': player})
 
 
-@require_POST()
+@require_POST
 def create_player(request):
-    if dict_has_keys(request.POST, ('username', 'email', 'password'), check_not_empty=True):
-        new_user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
-        new_player = Player(photo=Player.retrieve_default_avatar(), user=new_user)
-        return redirect(reverse("core:home"), player={'player': new_player})
-    else:
-        return render(request, "player_create.html", {'error': 'Username, email or password cannot be empty.'})
+    try:
+        if dict_has_keys(request.POST, ('username', 'email', 'password',), check_not_empty=True):
+            new_user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+            new_player = Player(photo=Player.retrieve_default_avatar(), user=new_user)
+            return redirect("core:player", username=new_player.user.username)
+        else:
+            return render(request, "murpi_core/register.html", {'error': 'Username, email or password cannot be empty.'})
+    except IntegrityError as ie:
+        return render(request, "murpi_core/register.html", {'error': 'Username ' + request.POST['username'] + ' is taken.'})
 
 
-@require_POST()
+@require_safe
+def show_create_player(request):
+    return render(request, "murpi_core/register.html")
+
+
+@require_POST
 def login(request):
     if dict_has_keys(request.POST, ('username', 'password'), check_not_empty=True):
         user = authenticate(username=request.POST['username'], password=request.POST['password'])
@@ -33,10 +44,15 @@ def login(request):
                 if player is not None:
                     return redirect(reverse("core:home"), player={'player': player})
                 else:
-                    render(request, "login.html", {'error': 'This login is not associated with a user. '
-                                                            'Please contact an administrator.'})
+                    render(request, "murpi_core/login.html", {'error': 'This login is not associated with a user. '
+                                                              'Please contact an administrator.'})
             else:
-                render(request, "login.html", {'error': 'That account is disabled!'})
+                render(request, "murpi_core/login.html", {'error': 'That account is disabled!'})
         else:
             # the authentication system was unable to verify the username and password
-            render(request, "login.html", {'error': 'That username or password is incorrect.'})
+            render(request, "murpi_core/login.html", {'error': 'That username or password is incorrect.'})
+
+
+@require_safe
+def show_login(request):
+    return render(request, "murpi_core/login.html")
