54a26a9e7256755e77a60974a96a0f4f5147d8ed
jacobbridges <him@jacobandkate143.com>
2015-05-16 13:21:46 -0400

    added urls for register and login pages; also changed the player url to use player_name

--- MURPI/urls.py
+++ MURPI/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -20,6 +22,8 @@ from murpi_core import views as core_views
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^player/([0-9]+)/$', core_views.player_details)
-]
+    url(r'^admin/$', include(admin.site.urls)),
+    url(r'^player/@(?P<username>[A-Za-z0-9_]+)/$', core_views.retrieve_player, name="core:player"),
+    url(r'^register/$', core_views.show_create_player),
+    url(r'^register_player/$', core_views.create_player)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
