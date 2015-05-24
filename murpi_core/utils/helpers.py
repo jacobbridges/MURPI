1282e5d446eadea635a3109211d8eb95015e241c
jacobbridges <him@jacobandkate143.com>
2015-05-24 14:19:33 -0400

    added method to delete unused uploaded files

--- murpi_core/utils/helpers.py
+++ murpi_core/utils/helpers.py
@@ -1,6 +1,6 @@
 from django.http.request import QueryDict
 from MURPI.settings import MEDIA_ROOT
-from os import remove
+import os
 
 
 def dict_has_keys(d, keys, check_not_empty=False):
@@ -26,10 +26,21 @@ def handle_uploaded_files(f, path_to_file):
             destination.write(chunk)
 
 
-def delete_uploaded_files(model, field, file_path):
+def delete_uploaded_file(model, field, file_path):
     args = {field: file_path}
     if not model.objects.filter(**args):
         try:
-            remove(MEDIA_ROOT + '/' + file_path)
+            os.remove(MEDIA_ROOT + '/' + file_path)
         except IOError:
             print u"Was not able to delete file: {}".format(file_path)
+
+
+def clean_uploaded_files():
+    from murpi_core.models import Player, Universe, World, Place
+    models_with_file_fields = [Player, Universe, World, Place]
+    for path, subdirs, files in os.walk(MEDIA_ROOT):
+        for name in files:
+            if not name.startswith('default') and (name.endswith('.jpg') or name.endswith('.png')):
+                if not any([model.has_file(os.path.join(path, name).split(MEDIA_ROOT)[-1].strip('/')) for model in models_with_file_fields]):
+                    print u"Deleting file {} from directory {}".format(name, path)
+                    os.remove(os.path.join(path, name))
